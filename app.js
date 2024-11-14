c7b44cf3d53d3c32da6e188b21775ced04d0bf6d
Katelyn <katelynqlam@gmail.com>
2024-11-14 13:02:16 -0600

    fix js file

--- app.js
+++ app.js
@@ -1,56 +1,69 @@
-// let Real_Madrid = {
-//   team_name: "Real Madrid",
-//   city: "Madrid",
-//   country: "Spain",
-//   top_scorers: ["Ronaldo", "Benzema", "Hazard"],
-//   world_wide_fans: 798,
-// };
+let Real_Madrid = {
+  team_name: "Real Madrid",
+  city: "Madrid",
+  country: "Spain",
+  top_scorers: ["Ronaldo", "Benzema", "Hazard"],
+  world_wide_fans: 798,
+};
 
-// let Barcelona = {
-//     team_name: "Barcelona",
-//     city: "Barcelona",
-//     country: "Spain",
-//     top_scorers: ["Messi", "Suarez", "Puyol"],
-//     world_wide_fans: 738,
-//   };
+// db.collection("teams").add(Real_Madrid);
 
-//   let Manchester_United = {
-//     team_name: "Manchester United",
-//     city: "Manchester",
-//     country: "England",
-//     top_scorers: ["Cantona", "Rooney", "Ronaldo"],
-//     world_wide_fans: 755,
-//   };
+let Barcelona = {
+  team_name: "Barcelona",
+  city: "Barcelona",
+  country: "Spain",
+  top_scorers: ["Messi", "Suarez", "Puyol"],
+  world_wide_fans: 738,
+};
 
-//   let Manchester_City = {
-//     team_name: "Manchester",
-//     city: "Manchester",
-//     country: "England",
-//     top_scorers: ["Sterling", "Aguero", "Haaland"],
-//     world_wide_fans: 537,
-//   };
+// db.collection("teams").add(Barcelona);
 
-//   let Brazil_National_Team = {
-//     team_name: "Brazil National Team",
-//     city: "N/A",
-//     country: "Brazil",
-//     top_scorers: ["Ronaldinho", "Cafu", "Bebeto"],
-//     world_wide_fans: 950,
-//   };
+let Manchester_United = {
+  team_name: "Manchester United",
+  city: "Manchester",
+  country: "England",
+  top_scorers: ["Cantona", "Rooney", "Ronaldo"],
+  world_wide_fans: 755,
+};
 
-//   let Argentina_national_team = {
-//     team_name: "Argentina national team",
-//     city: "N/A",
-//     country: "Argentina",
-//     top_scorers: ["Messi", "Batistuta", "Maradona"],
-//     world_wide_fans: 800,
-//   };
+// db.collection("teams").add(Manchester_United);
 
-// let Atletico_Madrid = {
-//     team_name: "Atletico Madrid",
-//     city: "Madrid",
-//     country: "Spain",
-//     top_scorers: ["Aragonés", "Griezmann", "Torez"],
-//     world_wide_fans: 400,
-//   };
-// db.collection("teams").add(Aragonés);
+let Manchester_City = {
+  team_name: "Manchester",
+  city: "Manchester",
+  country: "England",
+  top_scorers: ["Sterling", "Aguero", "Haaland"],
+  world_wide_fans: 537,
+};
+
+// db.collection("teams").add(Manchester_City);
+
+let Brazil_National_Team = {
+  team_name: "Brazil National Team",
+  city: "N/A",
+  country: "Brazil",
+  top_scorers: ["Ronaldinho", "Cafu", "Bebeto"],
+  world_wide_fans: 950,
+};
+
+// db.collection("teams").add(Brazil_National_Team);
+
+let Argentina_national_team = {
+  team_name: "Argentina national team",
+  city: "N/A",
+  country: "Argentina",
+  top_scorers: ["Messi", "Batistuta", "Maradona"],
+  world_wide_fans: 800,
+};
+
+// db.collection("teams").add(Argentina_national_team);
+
+let Atletico_Madrid = {
+  team_name: "Atletico Madrid",
+  city: "Madrid",
+  country: "Spain",
+  top_scorers: ["Aragonés", "Griezmann", "Torez"],
+  world_wide_fans: 400,
+};
+
+// db.collection("teams").add(Atletico_Madrid);
